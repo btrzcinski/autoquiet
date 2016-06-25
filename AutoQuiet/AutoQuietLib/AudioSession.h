649b51558c7b4bb25baef597de9bcc410f79aa74
Barnett Trzcinski <btrzcinski@gmail.com>
2016-06-25 13:24:54 -0700

    Removed dead methods

--- AutoQuiet/AutoQuietLib/AudioSession.h
+++ AutoQuiet/AutoQuietLib/AudioSession.h
@@ -11,38 +11,6 @@ namespace AutoQuietLib
     public ref class AudioSession
     {
     public:
-        static AudioSession^ GetFirstAudioSessionForProcess(System::Diagnostics::Process^ process)
-        {
-            CComPtr<IAudioSessionControl2> spSession;
-
-            auto hr = S_OK;
-            if (FAILED(hr = GetAudioSessionForProcessId(process->Id, &spSession))) {
-                auto exceptionMsg = System::String::Format(L"Error getting audio session for PID = {0}: 0x{1:X8}", process->Id, hr);
-                throw gcnew System::Exception(exceptionMsg);
-            }
-
-            if (spSession == nullptr) {
-                auto exceptionMsg = System::String::Format(L"No audio sessions open for PID = {0}: try playing audio first", process->Id);
-                throw gcnew System::Exception(exceptionMsg);
-            }
-
-            return gcnew AudioSession(spSession);
-        }
-
-        static bool TryGetFirstAudioSessionForProcess(System::Diagnostics::Process^ process, [System::Runtime::InteropServices::Out] AudioSession^% audioSession)
-        {
-            try
-            {
-                audioSession = GetFirstAudioSessionForProcess(process);
-            }
-            catch (System::Exception^)
-            {
-                return false;
-            }
-
-            return true;
-        }
-
         // Dispose
         ~AudioSession()
         {
